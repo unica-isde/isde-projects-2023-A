4dd4fa7f511309a44f95d45c2ba77aa3a9456b61
Salvatore Castello <s.castello@studenti.unica.it>
2023-11-11 16:20:04 +0100

    Added the test class for download button
    I have just created the class with 4 empty methods in it, next step will be implement the methods.

---
+++ app/tests/TestDownload.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestDownload(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def buttonsInInterface(self):
+        # Test if the buttons is in the interface and if it is clickable
+        pass
+
+    def jsonIsCorrect(self):
+        pass
+
+    def plotIsCorrect(self):
+        pass
+
