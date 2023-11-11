e3b553ca2972ab945b58f1700351ad314b23b7e8
Salvatore Castello <s.castello@studenti.unica.it>
2023-11-11 17:25:27 +0100

    Defined function for buttons in HTML page
    I have implemented the function that tests if buttons are inside the html page and if they have the correct text.

--- app/tests/TestDownload.py
+++ app/tests/TestDownload.py
@@ -1,18 +1,40 @@
 import unittest
+from bs4 import BeautifulSoup
 
 
 class TestDownload(unittest.TestCase):
-
-    def setUp(self):
-        pass
+    """
+    This class tests the file download functionalities of the application.
+    """
 
     def buttonsInInterface(self):
-        # Test if the buttons is in the interface and if it is clickable
-        pass
+        """
+        Test if the buttons are in the html page and have the right text.
+        """
+        with open("app/templates/classification_output.html") as f:
+            html = f.read()
+            soup = BeautifulSoup(html, "html.parser")
+
+            # Find the button with id "downloadResultsButton"
+            button = soup.find("button", {"id": "downloadResultsButton"})
+
+            # Check if the button is in the html page
+            self.assertIsNotNone(button, "There is no button with id 'download' in the html page")
+
+            # Check if the button has the right text
+            self.assertEqual(button.text, "Download results", "The button has not the right text")
+
+            # Find the button with id "downloadPlotButton"
+            button = soup.find("button", {"id": "downloadPlotButton"})
+
+            # Check if the button is in the html page
+            self.assertIsNotNone(button, "There is no button with id 'download' in the html page")
+
+            # Check if the button has the right text
+            self.assertEqual(button.text, "Download plot", "The button has not the right text")
 
     def jsonIsCorrect(self):
         pass
 
     def plotIsCorrect(self):
         pass
-
