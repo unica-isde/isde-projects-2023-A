8903cd9636af7f11ab34c065fc6c844e1ea8a3f3
Marco <pintore0000@gmail.com>
2023-11-11 20:53:21 +0100

    initial add of test_form
    test_image_loading will test the correct loading with post request

---
+++ app/tests/test_form.py
@@ -0,0 +1,12 @@
+import unittest
+import forms.classification_form
+
+class TestForm(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_image_loading(self):
+        pass
