a12a0873f708787971122430e30bd5f09ba67afb
Salvatore Castello <s.castello@studenti.unica.it>
2023-11-11 17:46:20 +0100

    Implemented buttons in the html
    With the previous defined test i have develope the buttons and made some edit in the test so they use the <a> tag instead of <button> with href, specified the research with bs4 with the "role" in button.
    Also changed the style of the buttons in the page.

--- app/tests/TestDownload.py
+++ app/tests/TestDownload.py
@@ -7,34 +7,34 @@ class TestDownload(unittest.TestCase):
     This class tests the file download functionalities of the application.
     """
 
-    def buttonsInInterface(self):
+    def test_buttonsInInterface(self):
         """
         Test if the buttons are in the html page and have the right text.
         """
-        with open("app/templates/classification_output.html") as f:
+        with open("../templates/classification_output.html") as f:
             html = f.read()
             soup = BeautifulSoup(html, "html.parser")
 
             # Find the button with id "downloadResultsButton"
-            button = soup.find("button", {"id": "downloadResultsButton"})
+            button = soup.find("a", {"id": "downloadResultsButton", "role": "button"})
 
-            # Check if the button is in the html page
-            self.assertIsNotNone(button, "There is no button with id 'download' in the html page")
+            # Check if the button with "a" tag and "role:button" is in the html page
+            self.assertIsNotNone(button, "There is no button with id 'downloadResultsButton' in the html page")
 
             # Check if the button has the right text
             self.assertEqual(button.text, "Download results", "The button has not the right text")
 
             # Find the button with id "downloadPlotButton"
-            button = soup.find("button", {"id": "downloadPlotButton"})
+            button = soup.find("a", {"id": "downloadPlotButton", "role": "button"})
 
             # Check if the button is in the html page
-            self.assertIsNotNone(button, "There is no button with id 'download' in the html page")
+            self.assertIsNotNone(button, "There is no button with id 'downloadPlotButton' in the html page")
 
             # Check if the button has the right text
             self.assertEqual(button.text, "Download plot", "The button has not the right text")
 
-    def jsonIsCorrect(self):
+    def test_jsonIsCorrect(self):
         pass
 
-    def plotIsCorrect(self):
+    def test_plotIsCorrect(self):
         pass
