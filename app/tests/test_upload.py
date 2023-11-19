ee345ba727ee06a6314056e8e6189d5891fd7fc9
Marco <pintore0000@gmail.com>
2023-11-19 12:18:26 +0100

    refactor and bug fix in test (still in progress)

--- app/tests/test_upload.py
+++ app/tests/test_upload.py
@@ -8,7 +8,8 @@ class TestUpload(unittest.TestCase):
         self.host = "localhost"
         self.port = "8000"
         self.api_name = "upload-and-classify"
-        self.path_image_to_test = "app/static/test_images/neural_net.png"
+        self.path_image_to_test = "app/static/test_images/"
+        self.image_filename = "neural_net.png"
 
     def tearDown(self):
         del self.host
@@ -23,24 +24,27 @@ class TestUpload(unittest.TestCase):
         """
         Test if the classified image is was uploaded correctly
         """
+        img_complete_path = self.path_image_to_test + self.image_filename
 
-        with open(self.path_image_to_test, "rb") as file:
+        with open(img_complete_path, "rb") as file:
             # Conversion to base64 format of test image
-            base64image = base64.b64encode(file.read())
+            img_raw_bytes = file.read()
 
-            # Prepare user-like form, with model_id and choosen file 
+            # Prepare user-like form, with model_id and choosen file
             form_data = {
                 "model_id": "inception_v3"
             }
 
-            files = {"image": (file.name, file, "image/jpeg")}
-            
+            files = {
+            "image": (self.path_image_to_test, img_raw_bytes, "image/jpeg"),
+            }
+            #print(img_raw_bytes)
             # Perform a http post request to localhost
             response = requests.post(
-                f"http://{self.host}:{self.port}/{self.api_name}", 
-                data = form_data, 
-                files = files)
-
+                f"http://{self.host}:{self.port}/{self.api_name}",
+                data = form_data,
+                files = files
+                )
             # Check if the request was successful
             self.assertEqual(response.status_code, 200, "The status code is not 200")
 
@@ -56,9 +60,12 @@ class TestUpload(unittest.TestCase):
             # Extract the base64 part from the src attribute
             _, base64_part = src_attribute.split(",", 1)
 
+            # Convert original image bytes to base 64
+            base64_image = base64.b64encode(img_raw_bytes).decode('utf-8')
+
             # Compare the base64 from the response with the original base64
-            self.assertEqual(base64_part, base64image, f"Error uploading the image in /{self.api_name} api")
-    
+            self.assertEqual(base64_part, base64_image, f"Error uploading the image in /{self.api_name} api")
+
 if __name__ == '__main__':
     unittest.main()
-        
+
